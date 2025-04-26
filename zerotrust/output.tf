97b1aa7efc532089549b29651ea646c071bac5a8
luminosita <milosh@emisia.net>
2025-04-26 09:28:08 +0200

    tunnel ID

--- zerotrust/output.tf
+++ zerotrust/output.tf
@@ -1,5 +1,6 @@
 output "result" {
   value = {
     gateway_certificate_id = length(data.cloudflare_zero_trust_gateway_certificates.gateway_certificates) > 0 ? data.cloudflare_zero_trust_gateway_certificates.gateway_certificates.result[0].id : cloudflare_zero_trust_gateway_certificate.gateway_certificate[0].id
+    tunnel_id              = cloudflare_zero_trust_tunnel_cloudflared.tunnel.id
   }
 }
